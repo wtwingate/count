2fb43384cd8b1cdd6f4f5c96ada333c7114a1ebb
William Wingate <wtwingate@gmail.com>
2024-04-24 11:09:22 -0400

    Implement word, line, and character counting

--- count.c
+++ count.c
@@ -1,34 +1,59 @@
 #include <stdio.h>
 
-void count(FILE *fp);
+#define TRUE   1
+#define FALSE  0
+
+unsigned char parse_args(int argc, char *argv[]);
+void count(FILE *fp, unsigned char opts, char *fname);
 
 int main(int argc, char *argv[])
 {
 	FILE *fp;
+	char *fname;
+	unsigned char opts;
 
 	if (argc == 1) {  /* no args; count standard input */
-		count(stdin);
+		count(stdin, 0, "");
 	} else {
-		fp = fopen(argv[1], "r");
+		fname = argv[1];
+		fp = fopen(fname, "r");
 		if (fp == NULL) {
-			printf("Error: can't open %s\n", argv[1]); 
+			printf("Error: can't open %s\n", fname); 
 			return 1;
 		} else {
-			count(fp);
+			count(fp, 0, fname);
 			fclose(fp);
 		}
 	}
 	return 0;
 }
 
-void count(FILE *fp)
+unsigned char parse_args(int argc, char *argv[])
+{
+	return 0;
+}
+
+void count(FILE *fp, unsigned char opts, char *fname)
 {
 	int c;
+	int in_word;
 	long words, lines, chars, bytes;
 
+	in_word = FALSE;
 	words = lines = chars = bytes = 0;
 	while ((c = getc(fp)) != EOF) {
 		bytes ++;
+		chars ++;
+		if (c == '\n') {
+			lines++;
+		}
+		if (c == ' ' || c == '\t' || c == '\n') {
+			in_word = FALSE;
+		} else if (in_word == FALSE) {
+			in_word = TRUE;
+			words++;
+		}
 	}
-	printf("%ld\n", bytes);
+
+	printf("%ld\t%ld\t%ld\t%s\n", lines, words, bytes, fname);
 }
